bd5fa235176fbc2707b450a4748b5c2043fa02e3
田剑 <tj1468195034@gmail.com>
2019-02-16 16:14:03 +0800

    dictionary

--- src/main/java/com/tianjian/rest/CodeManagerController.java
+++ src/main/java/com/tianjian/rest/CodeManagerController.java
@@ -12,12 +12,13 @@ import com.tianjian.service.ServiceEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/code")
 public class CodeManagerController {
 
     @Autowired
